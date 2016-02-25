c89ad7236576dc2061bcacea84ef7ce64248efc5
HQueerly <hqueerly@riseup.net>
2016-02-25 23:23:20 +0000

    rejiggled commandList for easier reference. changed command parsing to only accept valid commands. made small alteration to regex (with help from kat) to capture the three different command types properly. used 2 new array functions (suggested by kat) to filter and reindex the results of the regex.

--- php_resources/commandInterpreter.php
+++ php_resources/commandInterpreter.php
@@ -1,70 +1,80 @@
 <?php
 
 class commandInterpreter {
-	private $interactionObject = null;
+	private $localInteractionObject = null;
+	private $localCommandList = null;
 
-	public function __construct($intObject, $comList){
+	public function __construct($interactionObject, $commandList){
 		//takes the interaction object &
 		//the command list from settings.php
-		$this->interactionObject = $intObject;
-		$this->commandList = $comList;
+		$this->LocalInteractionObject = $interactionObject;
+		$this->localCommandList = $commandList;
 	}
 
 	//which command did the user input? use...with..., take, attack, etc
 	public function sliceInput($userInput, $outputString = null){
 		//turns input into array using regex of doom
-		//"^(.*?)(?:\s+|$)(?:(.*?)\s+(.*?)\s+(.*?)$|$)/i"
+		//"^(.*?)(?:\s+|$)(?:(.*?)\s+(.*?)\s+(.*?)$|(.*?)$|$)/i"
 
 		$list1 = "";
 		$list2 = "";
 
-		foreach($this->commandList["part1"] as $handle){
-			$list1 .= $handle;
+		foreach($this->localCommandList as $handle => $content){
+			foreach($this->localCommandList[$handle]["alias"] as $alias){
+			$list1 .= $alias;
 			$list1 .= "|";
-		}
-		$handle = null;
-
-		foreach($this->commandList["part2"] as $key => $handle){
-			if($this->commandList["part1"][$key] == $key){ //this if statement is a bit dodgy rn
-				$list2 .= $handle;
-				$list2 .= "|";
-				echo "\n SAUSAGES: ".$this->commandList["part1"][$key]."\n ".$key." ..END"
-			} else {
-				$list2 = "invalid;";
 			}
-
 		}
+		//$list1=substr($list1,0,strlen($list1)-1); //trim final pipe (might not be necessary)
 		$handle = null;
+
+		foreach($this->localCommandList as $handle => $content){
+			if(isset($this->localCommandList[$handle]["part2"])){
+				foreach($this->localCommandList[$handle]["part2"] as $alias){
+						$list2 .= $alias;
+						$list2 .= "|";
+				}
+			}
+		}
+
+		//$list2=substr($list2,0,strlen($list2)-1); //trim final pipe
+		$handle = null;
+
 
 		$list1 = preg_replace("/^(.*)\|$/i", "$1", $list1);
 		$list2 = preg_replace("/^(.*)\|$/i", "$1", $list2);
 
 		echo "\n".$list1."\n";
-		echo $list2."\n";
+		echo "\n".$list2."\n";
 
-		preg_match("/^(".$list1.")(?:\s+|$)(?:(.*?)\s+(".$list2.")\s+(.*?)$|$)/i", $userInput, $outputArray);
+		preg_match("/^(".$list1.")(?:\s+|$)(?:(.*?)\s+(".$list2.")\s+(.*?)$|(.*?)$|$)/i", $userInput, $outputArray);
+		$outputArray = array_values(array_filter($outputArray));
+		print_r($outputArray);
 
 		$command1 = "invalid";
 		$command2 = "invalid";
-
-		foreach ($this->commandList["part1"] as $handle){
-			if (isset($outputArray[1])){
-				if ($handle == $outputArray[1]){
-					$command1 = $handle;
+/*
+		foreach($this->localCommandList as $handle => $content){
+			foreach($this->localCommandList[$handle]["alias"] as $alias){
+				if (isset($outputArray[1])){
+					if ($alias == $outputArray[1]){
+						$command1 = $alias;
+					}
 				}
 			}
 		}
 
-		foreach ($this->commandList["part2"] as $handle){
+		foreach ($this->localCommandList["part2"] as $handle){
 			if (isset($outputArray[3])){
 				if ($handle == $outputArray[3]){
 					$command2 = $handle;
 				}
 			}
 		}
-
+*/
 		echo "\n SLICE: ".$command1." ".$command2."\n";
 
+		print_r($outputArray);
 		return $outputArray;
 
 	}
@@ -80,13 +90,15 @@ class commandInterpreter {
 		if(isset($commandArray[1])){
 			echo "command1 exists";
 			$command1 = "exists";
-			foreach ($this->commandList["part1"] as $key => $handle){
-				if ($commandArray[1] == $handle){
-					$command1 = "valid";
-					echo "\ncommand1 is ".$commandArray[1]." ".$handle;
+			foreach($this->localCommandList as $handle => $content){
+				foreach ($this->localCommandList[$handle]["alias"] as $alias){
+					if ($commandArray[1] == $alias){
+						$command1 = "valid";
+						echo "\ncommand1 is ".$commandArray[1]." ".$handle;
+						$command1handle = $handle;
+					}
 				}
 			}
-			$key = null; $handle = null;
 		}
 	
 		if(isset($commandArray[2])){
@@ -98,13 +110,20 @@ class commandInterpreter {
 		if(isset($commandArray[3])){
 			echo "\n command2 exists";
 			$command2 = "exists";
+			/*
 			foreach ($this->commandList["part2"] as $key => $handle){
 				if ($commandArray[3] == $handle){
 					$command2 = "valid";
 					echo "\n command2 is ".$commandArray[3]." ".$handle;
 				}
 			}
-			$key = null; $handle = null;
+			*/
+				foreach ($this->localCommandList[$command1handle]["part2"] as $alias){
+					if ($commandArray[3] == $alias){
+						$command2 = "valid";
+						echo "\ncommand2 is ".$commandArray[3]." ".$command1handle;
+					}
+				}
 		}
 
 		if(isset($commandArray[4])){
@@ -112,12 +131,36 @@ class commandInterpreter {
 			echo "\n parameter 2 exists and is ".$commandArray[4];
 		}
 
+		//defining the array that this function returns
+		$returnArray["valid"] = false;
+		$returnArray["parameterCount"] = null; //number of parameters found
+		$returnArray["commandCount"] = null;  //number of command words found
+		$returnArray["command"][1]["handle"] = null;
+		$returnArray["command"][1]["alias"] = null;
+		$returnArray["parameter"][1] =  null;
+		$returnArray["command"][2]["alias"] = null;
+		$returnArray["parameter"][2] = null;
+
+
+
 		if($command1 == "valid"  &&
 			$parameter1 == "exists" &&
 			$command2 == "valid" &&
 			$parameter2 == "exists"){
 			//valid 2 part command (must have 2 parameters)
 			echo "\n this is a valid 2 part command";
+
+			$returnArray["valid"] = true;
+			$returnArray["parameterCount"] = 2; //number of parameters found
+			$returnArray["commandCount"] = 2;  //number of command words found
+			$returnArray["command"][1]["handle"] = $command1handle;
+			$returnArray["command"][1]["alias"] = $commandArray[1];
+			$returnArray["parameter"][1] =  $commandArray[2];
+			$returnArray["command"][2]["alias"] = $commandArray[3];
+			$returnArray["parameter"][2] = $commandArray[4];
+			print_r($returnArray);
+			return $returnArray;
+
 		}
 
 		if($command1 == "valid"  &&
@@ -125,6 +168,17 @@ class commandInterpreter {
 				$command2 != "valid"){
 			//valid 1 part command with a parameter
 			echo "\n this is a valid 1 part command with a parameter";
+
+			$returnArray["valid"] = true;
+			$returnArray["parameterCount"] = 1; //number of parameters found
+			$returnArray["commandCount"] = 1;  //number of command words found
+			$returnArray["command"][1]["handle"] = $command1handle;
+			$returnArray["command"][1]["alias"] = $commandArray[1];
+			$returnArray["parameter"][1] =  $commandArray[2];
+			$returnArray["command"][2]["alias"] = null;
+			$returnArray["parameter"][2] = null;
+			print_r($returnArray);
+			return $returnArray;
 		}
 
 		if($command1 == "valid" &&
@@ -132,9 +186,21 @@ class commandInterpreter {
 			$command2 != "valid"){
 			//valid 1 part command with no parameter, e.g. "look"
 			echo "this is a valid 1 part command with no parameter";
-		}
 
+			$returnArray["valid"] = true;
+			$returnArray["parameterCount"] = 2; //number of parameters found
+			$returnArray["commandCount"] = 0;  //number of command words found
+			$returnArray["command"][1]["handle"] = $command1handle;
+			$returnArray["command"][1]["alias"] = $commandArray[1];
+			$returnArray["parameter"][1] =  null;
+			$returnArray["command"][2]["alias"] = null;
+			$returnArray["parameter"][2] = null;
+			print_r($returnArray);
+			return $returnArray;
+		}
+		print_r($returnArray);
 	}
+
 }
 
 ?>
--- php_resources/settings.php
+++ php_resources/settings.php
@@ -1,20 +1,68 @@
 <?php
 //SETTINGS FOR PHPTeA
 
+//use
+$settings["commandList"]["use"]["alias"][] = "use";
+$settings["commandList"]["use"]["alias"][] = "combine";
 
-$settings["commandList"]["part1"]["use"] = "use";
-$settings["commandList"]["part2"]["use"] = "with";
+$settings["commandList"]["use"]["part2"][] = "with";
 
-$settings["commandList"]["part1"]["look at"] = "look at";
+//look at
+$settings["commandList"]["look at"]["alias"][] = "look at";
+$settings["commandList"]["look at"]["part2"][] = "examine";
 
-$settings["commandList"]["part1"]["kick"] = "kick";
+//eat
+$settings["commandList"]["eat"]["alias"][] = "eat";
 
-$settings["commandList"]["part1"]["eat"] = "eat";
+//drink
+$settings["commandList"]["drink"]["alias"][] = "drink";
 
-$settings["commandList"]["part1"]["walk"] = "walk to";
+//open
+$settings["commandList"]["open"]["alias"][] = "open";
+$settings["commandList"]["open"]["part2"][] = "with";
 
-$settings["commandList"]["part1"]["attack"] = "attack";
-$settings["commandList"]["part2"]["attack"] = "with";
+//close
+$settings["commandList"]["close"]["alias"][] = "close";
+$settings["commandList"]["close"]["part2"][] = "with";
 
+//unlock
+$settings["commandList"]["unlock"]["alias"][] = "unlock";
+$settings["commandList"]["unlock"]["part2"][] = "with";
+
+//lock
+$settings["commandList"]["lock"]["alias"][] = "lock";
+$settings["commandList"]["lock"]["part2"][] = "with";
+
+//walk to
+$settings["commandList"]["walk"]["alias"][] = "walk to";
+$settings["commandList"]["walk"]["alias"][] = "go to";
+$settings["commandList"]["walk"]["alias"][] = "move to";
+$settings["commandList"]["walk"]["alias"][] = "travel to";
+$settings["commandList"]["walk"]["alias"][] = "walk";
+$settings["commandList"]["walk"]["alias"][] = "go";
+$settings["commandList"]["walk"]["alias"][] = "move";
+$settings["commandList"]["walk"]["alias"][] = "travel";
+
+//attack
+$settings["commandList"]["attack"]["alias"][] = "attack";
+$settings["commandList"]["attack"]["alias"][] = "kick";
+
+$settings["commandList"]["attack"]["part2"][] = "with";
+
+print_r($settings["commandList"]);
+
+
+$list1 = "";
+
+foreach($settings["commandList"] as $handle => $content){
+	foreach($settings["commandList"][$handle]["alias"] as $alias){
+	$list1 .= $alias;
+	$list1 .= "|";
+	}
+	
+}
+$list1=substr($list1,0,strlen($list1)-1);
+
+echo $list1;
 
 ?>
